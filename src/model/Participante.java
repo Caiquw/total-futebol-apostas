a3a1130ae5198cd65ab062a4cce206c89b5a5203
Caiquw <castro.caique@outlook.com>
2026-05-21 18:31:04 -0300

    add: Participante, Pessoa e Interface

--- src/model/Participante.java
+++ src/model/Participante.java
@@ -1,4 +1,45 @@
 package model;
 
-public class Participante {
+public class Participante extends Pessoa{
+    public int getPontuacaoTotal() {
+        return pontuacaoTotal;
+    }
+
+    public void setPontuacaoTotal(int pontuacaoTotal) {
+        this.pontuacaoTotal = pontuacaoTotal;
+    }
+
+    public Grupo getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(Grupo grupo) {
+        this.grupo = grupo;
+    }
+
+    private int pontuacaoTotal;
+    // grupo em que o participante faz parte
+    private Grupo grupo;
+
+    public Participante(String nome, String login) {
+        super(nome, login);
+        this.pontuacaoTotal = 0;
+    }
+
+    public String getTipo(){
+        return "Participante";
+    }
+
+    public Participante(){
+        super();
+        this.pontuacaoTotal = 0 ;
+    }
+
+    public void adicionarPontos(int pontos) {
+        this.pontuacaoTotal += pontos;
+    }
+    @Override
+    public String getTipoUsuario() {
+        return "";
+    }
 }
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,6 +1,49 @@
 package model;
 
-abstract class Pessoa {
+abstract class Pessoa implements Int_Exibir {
 
+    public Pessoa(String nome, String login) {
+        this.nome = nome;
+        this.login = login;
+    }
+
+    private String nome;
+    private String login;
+
+    public Pessoa() {
+        this.nome = "";
+        this.login = "";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    // identificar tipo no ADM ou PARTICIPANTE
+    public abstract String getTipoUsuario();
+
+
+    @Override
+    public String Exibir() {
+
+        return "[" + getTipoUsuario() + "] " + nome + " (login: " + login + ")";
+    }
+
+    @Override
+    public String toString(){
+        return nome;
+    }
 
 }
