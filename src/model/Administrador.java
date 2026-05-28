303619f71a8dfb96e4d665ec6b57ae8f45e41aae
luisaa32 <luisaburanelo.dev@gmail.com>
2026-05-28 11:45:58 -0300

    administrador

--- src/model/Administrador.java
+++ src/model/Administrador.java
@@ -1,6 +1,24 @@
 package model;
 
-public class Administrador {
+public class Administrador extends Pessoa {
 
+    // construtor
+    public Administrador(){
+        super(); //serve para chamar o constru de pessoa
+    }
+
+    public Administrador(String nome, String login)
+        super(nome, login);
+
+    @Override
+    public String getTipo() {   // no pessoa é abstrato os filhos que se virem - definiu string
+        return "Administrador"; // definiu que é um adm
+    }
+
+    @Override
+    public String exibirInfo()m {  // sobrescreve falando "adm carlos na hora de exibir
+        return "[ADMINISTRADOR] " + getNome();
+    }
 }
+
 
