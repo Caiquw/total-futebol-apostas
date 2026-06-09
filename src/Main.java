335f146587479ea427f069771456511963afe357
Caiquw <castro.caique@outlook.com>
2026-06-08 21:05:46 -0300

    backup

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-
+        InicializarDB.inicializar();
 
         SwingUtilities.invokeLater(LoginFrame::new);
     }
